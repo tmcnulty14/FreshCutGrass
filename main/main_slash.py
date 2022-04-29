d0a233820acb0cdb7575b36e3ae54d111afc2379
Thomas McNulty <tommcn.14@gmail.com>
2022-04-29 13:17:10 -0400

    Add event listening
    Add ready and message event listening.
    
    Sets a D&D related status every hour.
    Adds lemon emoji reactions to messages that mention Myron Lermontov.

--- main/main_slash.py
+++ main/main_slash.py
@@ -1,8 +1,11 @@
 import os
 import shlex
+from datetime import datetime
 
-from discord import Member
+import discord
+from discord import Member, Message
 from discord.ext import commands
+from discord.ext import tasks
 from discord_slash import SlashCommand, SlashContext
 from discord_slash.utils.manage_commands import create_option
 from dotenv import load_dotenv
@@ -13,6 +16,33 @@ GUILD_IDS = [834548590399586365, 933438152826306640, 771115921443651615]
 bot = commands.Bot(command_prefix="!")
 slash = SlashCommand(bot, sync_commands=True)
 
+
+@bot.event
+async def on_ready():
+    # Print debug info about the guilds the bot is active in.
+    print("FreshCutGrass is now active in " + str(len(bot.guilds)) + " guilds:")
+    for guild in bot.guilds:
+        print(f"- {guild.name} (id: {guild.id})")
+
+    await update_status()
+
+@bot.event
+async def on_message(message: Message):
+    # Lemon react to mentions of Myron Lermontov.
+    lemon_triggers = ["lermontov", "lairmontov"]
+    if any(trigger in message.content.lower() for trigger in lemon_triggers):
+        await message.add_reaction("🍋") # Lemon emoji
+
+@tasks.loop(hours=1)
+async def update_status():
+    now = datetime.now()
+    if (now.weekday() == 3 and now.hour >= 21) or (now.weekday() == 4 and now.hour <= 2):
+        print("Setting status to streaming twitch.tv/criticalrole")
+        await bot.change_presence(activity=discord.Streaming(name="Critical Role",
+                                                             url="https://www.twitch.tv/criticalrole"))
+    else:
+        print("Setting status to playing Dungeons & Dragons")
+        await bot.change_presence(activity=discord.Game("Dungeons and Dragons"))
 
 @slash.slash(
     name="hello",
