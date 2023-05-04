63252a09aeb42035b3688b658b434894d33db311
Thomas McNulty <tommcn.14@gmail.com>
2023-05-04 19:26:10 -0400

    Fix Python3.11 OpenSSL Installation
    Manually installs OpenSSL and links it to Python3.11 installation to fix Pip SSL issues.

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Installing Python3 manually to get 3.11 instead of 3.7
+# Amazon Linux repos contain Python3.7 - we want 3.11 instead
 # yum install -y python3, python3-pip
 
 if /usr/local/bin/python3.11 --version ; then
@@ -12,17 +12,41 @@ else
   yum -y groupinstall "Development Tools"
   yum install -y gcc openssl-devel bzip2-devel libffi-devel
 
+  # Download OpenSSL
+  mkdir /tmp/ssl
+  cd /tmp/ssl || exit
+  wget https://ftp.openssl.org/source/openssl-1.1.1q.tar.gz --no-check-certificate
+  tar xzf openssl-1.1.1q.tar.gz
+  cd openssl-1.1.1q || exit
+
+  # Install OpenSSL
+  ./config --prefix=/usr --openssldir=/etc/ssl --libdir=lib no-shared zlib-dynamic
+  make
+  # make test # Failing? Not sure why, doesn't seem to matter
+  make install
+
+  # Manual Verification that OpenSSL is installed
+  # openssl version
+  # which openssl
+
   # Download Python
-  cd /opt || exit
+  mkdir /tmp/Python311
+  cd /tmp/Python311 || exit
   wget https://www.python.org/ftp/python/3.11.3/Python-3.11.3.tgz
   tar xzf Python-3.11.3.tgz
+  cd Python-3.11.3 || exit
 
   # Build Python
-  cd Python-3.11.3 || exit
-  ./configure --enable-optimizations
+  ./configure --enable-optimizations --with-openssl=/usr
   make altinstall
 
-  rm -f /opt/Python-3.11.3.tgz
+  # Manual Verification that Python is installed
+  # python3 -V
+  # python -V
+
+  # Clean up Build Directories
+  rm -rf /tmp/ssl
+  rm -rf /tmp/Python311
 fi
 
 # Install python package dependencies
